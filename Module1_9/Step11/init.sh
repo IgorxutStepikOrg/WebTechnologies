17bea90ceebae2b4a2087482d25ebf2cd0d9bf94
igorxut <igorxut@gmail.com>
2016-11-04 18:07:20 +0300

    Create init.sh

---
+++ Module1_9/Step11/init.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+mkdir -p {/home/box/web/public/{img,css,js},/home/box/web/{uploads,etc}}
+cp /home/box/web/Module1_8/Step12/nginx.conf /home/box/web/etc/nginx.conf
+
+sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
+sudo /etc/init.d/nginx restart
