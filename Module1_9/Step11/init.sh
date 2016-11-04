228323c60c5bc06b5c56effd3adb2256ae7bf488
igorxut <igorxut@gmail.com>
2016-11-04 18:14:03 +0300

    Update init.sh

--- Module1_9/Step11/init.sh
+++ Module1_9/Step11/init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 mkdir -p {/home/box/web/public/{img,css,js},/home/box/web/{uploads,etc}}
-cp /home/box/web/Module1_8/Step12/nginx.conf /home/box/web/etc/nginx.conf
+cp /home/box/web/Module1_9/Step11/hello.py /home/box/web/hello.py
 
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
