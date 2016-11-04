5a7ad848fc2fb15687951be87927195114e68b61
igorxut <igorxut@gmail.com>
2016-11-04 18:33:40 +0300

    Update init.sh

--- Module1_9/Step11/init.sh
+++ Module1_9/Step11/init.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-mkdir -p {/home/box/web/public/{img,css,js},/home/box/web/{uploads,etc}}
+mkdir -p /home/box/web/etc
 cp /home/box/web/Module1_9/Step11/hello.py /home/box/web/hello.py
+cp /home/box/web/Module1_9/Step11/gunicorn.conf /home/box/web/etc/gunicorn.conf
 
-sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
+sudo ln -sf /home/box/web/Module1_9/Step11/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
+
+sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/default
+sudo /etc/init.d/gunicorn restart
