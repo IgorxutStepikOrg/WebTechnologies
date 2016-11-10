d3157a5a9259fb46b146fa626559c89a1b82a0d3
igorxut <igorxut@gmail.com>
2016-11-10 16:06:07 +0300

    Update urls.py

--- Module2_1/Step11/ask/qa/urls.py
+++ Module2_1/Step11/ask/qa/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import patterns, url
 
-urlpatterns = patterns('qa.views',
-                       url(r'^$', 'index', name='index'),
-                       url(r'^popular/$', 'popular', name='popular'),
-                       url(r'^question/(?P<slug>\w+)/$', 'question', name='question'),
-                       url(r'^ask/$', 'ask', name='ask'),
-                       url(r'^answer/$', 'answer', name='answer'),
-                       url(r'^signup/$', 'signup', name='signup'),
-                       url(r'^login/$', 'login', name='login'),
-                       )
+urlpatterns = patterns(
+  'qa.views',
+  url(r'^$', 'index', name='index'),
+  url(r'^login/$', 'login', name='login'),
+  url(r'^signup/$', 'signup', name='signup'),
+  url(r'^question/(?P<slug>\w+)/$', 'question', name='question'),
+  url(r'^ask/$', 'ask', name='ask'),
+  url(r'^popular/$', 'popular', name='popular'),
+  url(r'^new/$', 'new', name='new'),
+)
