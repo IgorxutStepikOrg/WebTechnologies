215b9fc5401ef913175b977140281ebf6835cd58
igorxut <igorxut@gmail.com>
2016-11-10 16:12:33 +0300

    Update urls.py

--- Module2_1/Step11/ask/ask/urls.py
+++ Module2_1/Step11/ask/ask/urls.py
@@ -1,4 +1,7 @@
 from django.conf.urls import url
 from django.contrib import admin
 
-urlpatterns = [url(r'^admin/', include(admin.site.urls)), url(r'^', include('qa.urls', namespace='qa')),]
+urlpatterns = [
+  url(r'^admin/', include(admin.site.urls)),
+  url(r'^', include('qa.urls', namespace='qa')),
+]
