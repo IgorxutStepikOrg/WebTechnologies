7b4cf40a2242488001c9216818eb20b8321a0a1f
igorxut <igorxut@gmail.com>
2016-11-10 18:47:11 +0300

    Update init.sh

--- Module2_1/Step11/init.sh
+++ Module2_1/Step11/init.sh
@@ -26,8 +26,12 @@ cp /home/box/web/Module2_1/Step11/ask/qa/views.py /home/box/web/ask/qa/views.py
 cp /home/box/web/Module2_1/Step11/ask/qa/urls.py /home/box/web/ask/qa/urls.py
 cp /home/box/web/Module2_1/Step11/ask/ask/urls.py /home/box/web/ask/ask/urls.py
 
+cat /home/box/web/ask/ask/settings.py | sed 's/\'django.contrib.staticfiles\',/\'django.contrib.staticfiles\',\n\'qa\',/' > test && sudo mv test /usr/bin/gunicorn_paster
+
 sudo ln -sf /home/box/web/Module2_1/Step11/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 
 sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/default
 sudo /etc/init.d/gunicorn restart
+
+
