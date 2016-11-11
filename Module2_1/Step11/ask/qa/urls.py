e2c3a651cf7ad9ef050d3994a003b098658d9056
igorxut <igorxut@gmail.com>
2016-11-11 13:14:48 +0300

    Update urls.py

--- Module2_1/Step11/ask/qa/urls.py
+++ Module2_1/Step11/ask/qa/urls.py
@@ -3,10 +3,10 @@ from qa.views import *
 
 urlpatterns = patterns(
   url(r'^$', 'index', name='index'),
-  url(r'^login/$', 'login', name='login'),
-  url(r'^signup/$', 'signup', name='signup'),
-  url(r'^question/(?P<slug>\w+)/$', 'question', name='question'),
-  url(r'^ask/$', 'ask', name='ask'),
-  url(r'^popular/$', 'popular', name='popular'),
-  url(r'^new/$', 'new', name='new'),
+  url(r'^login/', 'login', name='login'),
+  url(r'^signup/', 'signup', name='signup'),
+  url(r'^question/(?P<slug>\w+)/', 'question', name='question'),
+  url(r'^ask/', 'ask', name='ask'),
+  url(r'^popular/', 'popular', name='popular'),
+  url(r'^new/', 'new', name='new'),
 )
