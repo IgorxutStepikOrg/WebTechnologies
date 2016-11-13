b67b407f17c92fb723c515140bb2bf445bb9f1c9
igorxut <igorxut@gmail.com>
2016-11-13 16:42:10 +0300

    Update init.sh

--- Module2_3/Step10/init.sh
+++ Module2_3/Step10/init.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-sudo /etc/init.d/mysql start
-mysql -uroot -e "CREATE DATABASE db_ask;"
-mysql -uroot -e "CREATE USER 'user'@'localhost' IDENTIFIED BY 'password';"
-mysql -uroot -e "GRANT ALL ON db_ask.* TO 'user'@'localhost';"
-mysql -uroot -e "FLUSH PRIVILEGES;"
-
 mkdir -p {/home/box/web/public/{img,css,js},/home/box/web/{uploads,etc}}
 
 cd /home/box/web
@@ -21,13 +15,18 @@ cat /home/box/web/ask/ask/settings.py | sed "s/'django.contrib.staticfiles',/'dj
 cat /home/box/web/ask/ask/settings.py | sed "s/'ENGINE': 'django.db.backends.sqlite3',/'ENGINE': 'django.db.backends.mysql',/" > temp_txt && sudo mv temp_txt /home/box/web/ask/ask/settings.py
 cat /home/box/web/ask/ask/settings.py | sed "s/'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),/'NAME': 'db_ask',\n        'USER': 'user',\n        'PASSWORD': 'password',\n        'HOST': 'localhost',\n        'PORT': 3306,/" > temp_txt && sudo mv temp_txt /home/box/web/ask/ask/settings.py
 
+sudo /etc/init.d/mysql start
+mysql -uroot -e "CREATE DATABASE db_ask;"
+mysql -uroot -e "CREATE USER 'user'@'localhost' IDENTIFIED BY 'password';"
+mysql -uroot -e "GRANT ALL PRIVILEGES ON db_ask.* TO 'user'@'localhost';"
+
+cd /home/box/web/ask
+python manage.py syncdb
+
 sudo ln -sf /home/box/web/Module2_3/Step10/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 
 sudo ln -sf /home/box/web/Module2_3/Step10/gunicorn.conf /etc/gunicorn.d/default
 sudo /etc/init.d/gunicorn restart
-
-cd /home/box/web/ask
-python manage.py syncdb
 
 #end
