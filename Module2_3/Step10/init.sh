c92b2b08f9cc79933a8e35bab97ca3ac86ecee73
igorxut <igorxut@gmail.com>
2016-11-13 16:14:49 +0300

    Update init.sh

--- Module2_3/Step10/init.sh
+++ Module2_3/Step10/init.sh
@@ -18,7 +18,8 @@ cp /home/box/web/Module2_3/Step10/ask/qa/views.py /home/box/web/ask/qa/views.py
 cp /home/box/web/Module2_3/Step10/ask/ask/urls.py /home/box/web/ask/ask/urls.py
 
 cat /home/box/web/ask/ask/settings.py | sed "s/'django.contrib.staticfiles',/'django.contrib.staticfiles',\n    'qa',\n/" > temp_txt && sudo mv temp_txt /home/box/web/ask/ask/settings.py
-cat /home/box/web/ask/ask/settings.py | sed "s/'ENGINE': 'django.db.backends.sqlite3',\r\n        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),/'ENGINE': 'django.db.backends.mysql',\n            'NAME': 'db_ask',\n            'USER': 'django',\n            'PASSWORD': 'pass',\n            'HOST': localhost,\n            'PORT': '3306',\n/" > temp_txt && sudo mv temp_txt /home/box/web/ask/ask/settings.py
+cat /home/box/web/ask/ask/settings.py | sed "s/'ENGINE': 'django.db.backends.sqlite3',/'ENGINE': 'django.db.backends.mysql',\n/" > temp_txt && sudo mv temp_txt /home/box/web/ask/ask/settings.py
+cat /home/box/web/ask/ask/settings.py | sed "s/'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),/'NAME': 'db_ask',\n            'USER': 'django',\n            'PASSWORD': 'pass',\n            'HOST': localhost,\n            'PORT': '3306',/" > temp_txt && sudo mv temp_txt /home/box/web/ask/ask/settings.py
 
 sudo ln -sf /home/box/web/Module2_3/Step10/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
