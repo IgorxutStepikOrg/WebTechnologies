9dec334b1e6caaa446a60d653a055e92f9e8fd9a
igorxut <igorxut@gmail.com>
2016-11-13 17:39:48 +0300

    Update init.sh

--- Module2_3/Step10/init.sh
+++ Module2_3/Step10/init.sh
@@ -25,11 +25,7 @@ sudo ln -sf /home/box/web/Module2_3/Step10/gunicorn.conf /etc/gunicorn.d/default
 sudo /etc/init.d/gunicorn restart
 
 sudo /etc/init.d/mysql start
-mysql -uroot -e "
-  CREATE DATABASE db_ask;
-  CREATE USER 'user'@'localhost' IDENTIFIED BY 'password';
-  GRANT ALL ON db_ask.* TO 'user'@'localhost';
-"
+mysql -uroot -e "CREATE DATABASE 'db_ask'; CREATE USER 'user' IDENTIFIED BY 'password'; GRANT ALL ON db_ask.* TO 'user';"
 
 cd /home/box/web/ask/
 python manage.py syncdb
