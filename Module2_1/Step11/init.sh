e49ae5b9ca037916e2a42dc58913ffe6f8d5fdfd
igorxut <igorxut@gmail.com>
2016-11-13 13:07:44 +0300

    Update init.sh

--- Module2_1/Step11/init.sh
+++ Module2_1/Step11/init.sh
@@ -13,7 +13,7 @@ cd /home/box/web
 django-admin startproject ask
 
 cd /home/box/web/ask
-python manage.py startapp qa
+python3 manage.py startapp qa
 
 cp /home/box/web/Module2_1/Step11/ask/qa/views.py /home/box/web/ask/qa/views.py
 cp /home/box/web/Module2_1/Step11/ask/ask/urls.py /home/box/web/ask/ask/urls.py
