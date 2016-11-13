704623b0b1b17405aaf458494fa5efacc485ad68
igorxut <igorxut@gmail.com>
2016-11-13 13:04:40 +0300

    Update init.sh

--- Module2_1/Step11/init.sh
+++ Module2_1/Step11/init.sh
@@ -2,7 +2,6 @@
 
 sudo apt-get -y update
 
-sudo apt-get -y remove django
 sudo apt-get -y remove gunicorn
 
 sudo pip3 install django
@@ -17,13 +16,11 @@ cd /home/box/web/ask
 python manage.py startapp qa
 
 cp /home/box/web/Module2_1/Step11/ask/qa/views.py /home/box/web/ask/qa/views.py
-cp /home/box/web/Module2_1/Step11/ask/qa/urls.py /home/box/web/ask/qa/urls.py
 cp /home/box/web/Module2_1/Step11/ask/ask/urls.py /home/box/web/ask/ask/urls.py
 
 cat /home/box/web/ask/ask/settings.py | sed "s/'django.contrib.staticfiles',/'django.contrib.staticfiles',\n    'qa',\n/" > test && sudo mv test /home/box/web/ask/ask/settings.py
 
 sudo ln -sf /home/box/web/Module2_1/Step11/nginx.conf  /etc/nginx/sites-enabled/default
-# sudo ln -sf /home/box/web/Module2_1/Step11/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 
 sudo ln -sf /home/box/web/Module2_1/Step11/gunicorn.conf /etc/gunicorn.d/default
