577d2f48fb969f562a97787d535e3a5e0ae9afe3
igorxut <igorxut@gmail.com>
2016-11-13 15:22:46 +0300

    Update init.sh

--- Module2_3/Step10/init.sh
+++ Module2_3/Step10/init.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 sudo /etc/init.d/mysql start
+mysql -uroot -e "CREATE DATABASE db_ask;"
+mysql -uroot -e "CREATE USER 'django@localhost' IDENTIFIED BY 'pass';"
+mysql -uroot -e "GRANT ALL ON db_ask.* TO 'django@localhost';"
+mysql -uroot -e "FLUSH PRIVILEGES;"
 
 mkdir -p {/home/box/web/public/{img,css,js},/home/box/web/{uploads,etc}}
 
