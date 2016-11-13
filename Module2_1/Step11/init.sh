ebf80c1d0e1206856324c1a39d54ae84af72c3d0
igorxut <igorxut@gmail.com>
2016-11-13 13:05:07 +0300

    Update init.sh

--- Module2_1/Step11/init.sh
+++ Module2_1/Step11/init.sh
@@ -18,7 +18,7 @@ python manage.py startapp qa
 cp /home/box/web/Module2_1/Step11/ask/qa/views.py /home/box/web/ask/qa/views.py
 cp /home/box/web/Module2_1/Step11/ask/ask/urls.py /home/box/web/ask/ask/urls.py
 
-cat /home/box/web/ask/ask/settings.py | sed "s/'django.contrib.staticfiles',/'django.contrib.staticfiles',\n    'qa',\n/" > test && sudo mv test /home/box/web/ask/ask/settings.py
+cat /home/box/web/ask/ask/settings.py | sed "s/'django.contrib.staticfiles',/'django.contrib.staticfiles',\n    'qa',\n/" > temp_txt && sudo mv temp_txt /home/box/web/ask/ask/settings.py
 
 sudo ln -sf /home/box/web/Module2_1/Step11/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
