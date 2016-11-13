8c1baec076628ce85830fdf73df55cc16273ded5
igorxut <igorxut@gmail.com>
2016-11-13 13:17:47 +0300

    Update urls.py

--- Module2_1/Step11/ask/ask/urls.py
+++ Module2_1/Step11/ask/ask/urls.py
@@ -1,18 +1,14 @@
 from django.conf.urls import url, include, patterns
 from django.contrib import admin
-from qa.views import
 
 admin.autodiscover()
 
-urlpatterns = patterns(
-  url(r'^$', 'index', name='index'),
-  url(r'^login/', 'login', name='login'),
-  url(r'^signup/', 'signup', name='signup'),
-  url(r'^question/', include('qa.urls')),
-  url(r'^ask/', 'ask', name='ask'),
-  url(r'^popular/', 'popular', name='popular'),
-  url(r'^new/', 'new', name='new'),
-  
-  url(r'^admin/', admin.site.urls),
-  url(r'^', include('qa.urls')),
-)
+urlpatterns = [
+    url(r'^$', 'qa.views.test'),
+    url(r'^login/$', 'qa.views.test'),
+    url(r'^signup/$', 'qa.views.test'),
+    url(r'^question/(\d+)$', 'qa.views.test'),
+    url(r'^ask/.*$', 'qa.views.test'),
+    url(r'^popular/$', 'qa.views.test'),
+    url(r'^new/$', 'qa.views.test'),
+]
