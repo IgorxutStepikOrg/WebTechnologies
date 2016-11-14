85129f4b819ae7ee44454ccee5268d9382cfcddb
igorxut <igorxut@gmail.com>
2016-11-14 17:17:15 +0300

    Update init.sh

--- Module2_5/Step8/init.sh
+++ Module2_5/Step8/init.sh
@@ -13,13 +13,13 @@ mkdir -p /home/box/web/ask/templates
 cp /home/box/web/Module2_5/Step8/ask/ask/urls.py /home/box/web/ask/ask/urls.py
 cp /home/box/web/Module2_5/Step8/ask/qa/forms.py /home/box/web/ask/qa/forms.py
 cp /home/box/web/Module2_5/Step8/ask/qa/models.py /home/box/web/ask/qa/models.py
+cp /home/box/web/Module2_5/Step8/ask/qa/urls.py /home/box/web/ask/qa/urls.py
 cp /home/box/web/Module2_5/Step8/ask/qa/views.py /home/box/web/ask/qa/views.py
 cp /home/box/web/Module2_5/Step8/ask/templates/list.html /home/box/web/ask/templates/list.html
 cp /home/box/web/Module2_5/Step8/ask/templates/question.html /home/box/web/ask/templates/question.html
 
 cat /home/box/web/ask/ask/settings.py | sed "s/WSGI_APPLICATION = 'ask.wsgi.application'/WSGI_APPLICATION = 'ask.wsgi.application'\n\nTEMPLATE_DIRS = BASE_DIR + '\/templates'/" > temp_txt && sudo mv temp_txt /home/box/web/ask/ask/settings.py
 cat /home/box/web/ask/ask/settings.py | sed "s/'django.contrib.staticfiles',/'django.contrib.staticfiles',\n    'qa',/" > temp_txt && sudo mv temp_txt /home/box/web/ask/ask/settings.py
-#cat /home/box/web/ask/ask/settings.py | sed "s/'django.contrib.admin',/'qa',\n    'django.contrib.admin',/" > temp_txt && sudo mv temp_txt /home/box/web/ask/ask/settings.py
 cat /home/box/web/ask/ask/settings.py | sed "s/'ENGINE': 'django.db.backends.sqlite3',/'ENGINE': 'django.db.backends.mysql',/" > temp_txt && sudo mv temp_txt /home/box/web/ask/ask/settings.py
 cat /home/box/web/ask/ask/settings.py | sed "s/'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),/'NAME': 'db_ask',\n        'USER': 'user_ask',\n        'PASSWORD': 'pass_ask',\n        'HOST': 'localhost',\n        'PORT': '',/" > temp_txt && sudo mv temp_txt /home/box/web/ask/ask/settings.py
 
