5bee3cf3d441ce95b36fa880fb4db7ce5069b43b
igorxut <igorxut@gmail.com>
2016-11-14 11:38:17 +0300

    Create models.py

---
+++ Module2_5/Step8/ask/qa/models.py
@@ -0,0 +1,55 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class QuestionManager(models.Manager):
+
+    def new(self):
+        return self.order_by('-added_at')[:2]
+    
+    def popular(self):
+        return self.order_by('-rating')
+
+class Question(models.Model):
+    title = models.CharField(
+        default="",
+        max_length=255
+    )
+    text = models.TextField(
+        default=""
+    )
+    added_at = models.DateTimeField(
+        auto_now_add=True
+    )
+    rating = models.IntegerField(
+        default=0
+    )
+    author = models.ForeignKey(
+        User,
+        related_name="question_author"
+    )
+    likes = models.ManyToManyField(
+        User,
+        related_name="question_like"
+    )
+    objects = QuestionManager()
+
+    def __unicode__(self):
+        return self.title
+
+class Answer(models.Model):
+    text = models.TextField(
+        default=""
+    )
+    added_at = models.DateTimeField(
+        auto_now_add=True
+    )
+    question = models.ForeignKey(
+        Question
+    )
+    author = models.ForeignKey(
+        User,
+        related_name='answer_author'
+    )
+
+    def __unicode__(self):
+        return self.text
