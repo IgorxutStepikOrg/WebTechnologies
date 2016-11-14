534ab1e3f4c2452ecaaa0acee21671584c2ce422
igorxut <igorxut@gmail.com>
2016-11-14 23:06:17 +0300

    Update models.py

--- Module2_3/Step10/ask/qa/models.py
+++ Module2_3/Step10/ask/qa/models.py
@@ -26,7 +26,7 @@ class Question(models.Model):
     )
     author = models.ForeignKey(
         User,
-        related_name='question_author'
+        related_name="question_author"
     )
     likes = models.ManyToManyField(
         User,
@@ -38,11 +38,11 @@ class Question(models.Model):
 class Likes(models.Model):
     question = models.ForeignKey(
         Question,
-        related_name='like_question'
+        related_name="like_question"
     )
     user = models.ForeignKey(
         User,
-        related_name='like_user'
+        related_name="like_user"
     )
     date = models.DateTimeField(
         auto_now_add=True
@@ -61,5 +61,5 @@ class Answer(models.Model):
     )
     author = models.ForeignKey(
         User,
-        related_name='answer_author'
+        related_name="answer_author"
     )
