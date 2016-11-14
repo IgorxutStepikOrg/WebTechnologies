35962672045b36bd1d95938275d753244026f3b8
igorxut <igorxut@gmail.com>
2016-11-14 20:31:38 +0300

    Create urls.py

---
+++ Module2_6/Step11/ask/ask/urls.py
@@ -0,0 +1,16 @@
+from django.conf.urls import url, include
+from django.contrib import admin
+
+from qa.views import index, popular
+
+admin.autodiscover()
+
+urlpatterns = [
+    url(r'^$', index),
+    url(r'^login/', 'qa.views.test'),
+    url(r'^signup/', 'qa.views.test'),
+    url(r'^question/', include('qa.urls')),
+    url(r'^ask/', 'qa.views.test'),
+    url(r'^popular/', popular),
+    url(r'^new/', 'qa.views.test')
+]
