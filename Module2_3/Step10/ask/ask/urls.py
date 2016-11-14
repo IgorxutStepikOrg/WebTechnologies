14df3a5fd1928ab1e8d488322db2af34f483cb68
igorxut <igorxut@gmail.com>
2016-11-14 23:26:37 +0300

    Update urls.py

--- Module2_3/Step10/ask/ask/urls.py
+++ Module2_3/Step10/ask/ask/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     url(r"^$", test),
     url(r"^login/", test),
     url(r"^signup/", test),
-    url(r"^question/", include("qa.urls")),
+    url(r"^question/", include('qa.urls')),
     url(r"^ask/", test),
     url(r"^popular/", test),
     url(r"^new/", test)
