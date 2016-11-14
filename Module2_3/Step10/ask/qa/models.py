d0ee24c0a9e2f9cc91f2ffaff59ea22bd33ebbc1
igorxut <igorxut@gmail.com>
2016-11-14 23:07:51 +0300

    Update models.py

--- Module2_3/Step10/ask/qa/models.py
+++ Module2_3/Step10/ask/qa/models.py
@@ -13,7 +13,8 @@ class QuestionManager(models.Manager):
 
 class Question(models.Model):
     title = models.CharField(
-        default=""
+        default="",
+        max_length=255
     )
     text = models.TextField(
         default=""
