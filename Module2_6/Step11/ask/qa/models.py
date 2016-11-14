09567853807bf7b003aa04ff70486523735b7251
igorxut <igorxut@gmail.com>
2016-11-14 20:32:35 +0300

    Create models.py

---
+++ Module2_6/Step11/ask/qa/models.py
@@ -0,0 +1,65 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class QuestionManager(models.Manager):
+
+    def new(self):
+        return self.order_by('-added_at')
+    
+    def popular(self):
+        return self.order_by('-rating')
+
+class Question(models.Model):
+    title = models.CharField(
+        default="",
+        max_length=255
+    )
+    text = models.TextField(
+        default=""
+    )
+    added_at = models.DateTimeField(
+        auto_now_add=True
+    )
+    rating = models.IntegerField(
+        default=0
+    )
+    author = models.ForeignKey(
+        User,
+        related_name="question_author"
+    )
+    likes = models.ManyToManyField(
+        User,
+        through = 'Likes'
+    )
+    objects = QuestionManager()
+
+    def get_url(self):
+        return "/question/{}/".format(self.id)
+
+class Likes(models.Model):
+    question = models.ForeignKey(
+        Question,
+        related_name="like_question"
+    )
+    user = models.ForeignKey(
+        User,
+        related_name="like_user"
+    )
+    date = models.DateTimeField(
+        auto_now_add=True
+    )
+
+class Answer(models.Model):
+    text = models.TextField(
+        default=""
+    )
+    added_at = models.DateTimeField(
+        auto_now_add=True
+    )
+    question = models.ForeignKey(
+        Question
+    )
+    author = models.ForeignKey(
+        User,
+        related_name='answer_author'
+    )
