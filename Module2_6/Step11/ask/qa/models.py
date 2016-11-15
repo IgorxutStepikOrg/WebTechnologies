51b2e37f46c957afbb44d2f26e69c69cb3e45f8d
igorxut <igorxut@gmail.com>
2016-11-15 13:34:35 +0300

    Update models.py

--- Module2_6/Step11/ask/qa/models.py
+++ Module2_6/Step11/ask/qa/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class QuestionManager(models.Manager):
 
     def new(self):
@@ -9,57 +10,80 @@ class QuestionManager(models.Manager):
     def popular(self):
         return self.order_by('-rating')
 
+
 class Question(models.Model):
+
     title = models.CharField(
+        blank=False,
         default="",
         max_length=255
     )
     text = models.TextField(
+        blank=False,
         default=""
     )
     added_at = models.DateTimeField(
-        auto_now_add=True
+        auto_now_add=True,
+        blank=False
     )
     rating = models.IntegerField(
+        blank=False,
         default=0
     )
     author = models.ForeignKey(
         User,
+        blank=False,
         related_name="question_author"
     )
     likes = models.ManyToManyField(
         User,
-        through = 'Likes'
+        blank=True,
+        through='Likes'
     )
     objects = QuestionManager()
-
+    
     def get_url(self):
-        return "/question/{}/".format(self.id)
+        return "/question/{0}/".format(self.id)
+
+    def __unicode__(self):
+        return self.text
+
 
 class Likes(models.Model):
     question = models.ForeignKey(
         Question,
+        blank=False,
         related_name="like_question"
     )
     user = models.ForeignKey(
         User,
+        blank=False,
         related_name="like_user"
     )
     date = models.DateTimeField(
-        auto_now_add=True
+        auto_now_add=True,
+        blank=False
     )
+
 
 class Answer(models.Model):
     text = models.TextField(
+        blank=False,
         default=""
     )
     added_at = models.DateTimeField(
-        auto_now_add=True
+        auto_now_add=True,
+        blank=False
     )
     question = models.ForeignKey(
-        Question
+        Question,
+        blank=False
     )
     author = models.ForeignKey(
         User,
-        related_name='answer_author'
+        blank=False,
+        related_name="answer_author"
     )
+    
+    def __unicode__(self):
+        return self.text
