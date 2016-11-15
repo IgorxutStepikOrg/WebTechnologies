290ddbf103379227058ad63a5482715a0d64199f
igorxut <igorxut@gmail.com>
2016-11-15 16:49:21 +0300

    Update init.sh

--- Module2_6/Step11/init.sh
+++ Module2_6/Step11/init.sh
@@ -20,8 +20,8 @@ cp /home/box/web/Module2_6/Step11/ask/templates/question.html /home/box/web/ask/
 
 cat /home/box/web/ask/ask/settings.py | sed "s/WSGI_APPLICATION = 'ask.wsgi.application'/WSGI_APPLICATION = 'ask.wsgi.application'\n\nTEMPLATE_DIRS = BASE_DIR + '\/templates'/" > temp_txt && sudo mv temp_txt /home/box/web/ask/ask/settings.py
 cat /home/box/web/ask/ask/settings.py | sed "s/'django.contrib.staticfiles',/'django.contrib.staticfiles',\n    'qa',/" > temp_txt && sudo mv temp_txt /home/box/web/ask/ask/settings.py
-cat /home/box/web/ask/ask/settings.py | sed "s/'ENGINE': 'django.db.backends.sqlite3',/'ENGINE': 'django.db.backends.mysql',/" > temp_txt && sudo mv temp_txt /home/box/web/ask/ask/settings.py
-cat /home/box/web/ask/ask/settings.py | sed "s/'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),/'NAME': 'db_ask',\n        'USER': 'user_ask',\n        'PASSWORD': 'pass_ask',\n        'HOST': 'localhost',\n        'PORT': '',/" > temp_txt && sudo mv temp_txt /home/box/web/ask/ask/settings.py
+#cat /home/box/web/ask/ask/settings.py | sed "s/'ENGINE': 'django.db.backends.sqlite3',/'ENGINE': 'django.db.backends.mysql',/" > temp_txt && sudo mv temp_txt /home/box/web/ask/ask/settings.py
+#cat /home/box/web/ask/ask/settings.py | sed "s/'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),/'NAME': 'db_ask',\n        'USER': 'user_ask',\n        'PASSWORD': 'pass_ask',\n        'HOST': 'localhost',\n        'PORT': '',/" > temp_txt && sudo mv temp_txt /home/box/web/ask/ask/settings.py
 
 sudo ln -sf /home/box/web/Module2_6/Step11/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
@@ -29,11 +29,11 @@ sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/Module2_6/Step11/gunicorn.conf /etc/gunicorn.d/default
 sudo /etc/init.d/gunicorn restart
 
-sudo /etc/init.d/mysql start
-mysql -u root -e "CREATE DATABASE db_ask DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;"
-mysql -u root -e "CREATE USER 'user_ask' IDENTIFIED BY 'pass_ask';"
-mysql -u root -e "GRANT ALL PRIVILEGES ON db_ask.* TO 'user_ask';"
-mysql -u root -e "FLUSH PRIVILEGES;"
+#sudo /etc/init.d/mysql start
+#mysql -u root -e "CREATE DATABASE db_ask DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;"
+#mysql -u root -e "CREATE USER 'user_ask' IDENTIFIED BY 'pass_ask';"
+#mysql -u root -e "GRANT ALL PRIVILEGES ON db_ask.* TO 'user_ask';"
+#mysql -u root -e "FLUSH PRIVILEGES;"
 
 cd /home/box/web/ask/
 sudo python manage.py syncdb
