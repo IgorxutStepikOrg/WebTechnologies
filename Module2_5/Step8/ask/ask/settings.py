631d4bd05a84cfef245ee6e8212ac0821c989f0e
igorxut <igorxut@gmail.com>
2016-11-15 23:39:44 +0300

    Create settings.py

---
+++ Module2_5/Step8/ask/ask/settings.py
@@ -0,0 +1,57 @@
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+SECRET_KEY = "g!)autj10yz8+cc(@z_x+orw(*r)401^b^@ya()_3&vm0%5eo7"
+
+DEBUG = True
+
+TEMPLATE_DEBUG = True
+
+ALLOWED_HOSTS = []
+
+INSTALLED_APPS = [
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "qa",
+]
+
+MIDDLEWARE_CLASSES = [
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+
+ROOT_URLCONF = "ask.urls"
+
+WSGI_APPLICATION = "ask.wsgi.application"
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "db_ask",
+        "USER": "user_ask",
+        "PASSWORD": "pass_ask",
+        "HOST": "localhost",
+        "PORT": "",
+    }
+}
+
+LANGUAGE_CODE = "en-us"
+
+TIME_ZONE = "UTC"
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+STATIC_URL = "/static/"
