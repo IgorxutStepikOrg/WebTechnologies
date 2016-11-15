43e9a0fa27f52c038a3a86a9c111c5a4d99c5e22
igorxut <igorxut@gmail.com>
2016-11-16 00:18:14 +0300

    Update views.py

--- Module2_5/Step8/ask/qa/views.py
+++ Module2_5/Step8/ask/qa/views.py
@@ -3,7 +3,6 @@ from django.http import HttpResponse, Http404
 from django.core.paginator import Paginator
 
 from qa.models import Question, Answer
-from qa.forms import AskForm, AnswerForm
 
 
 def test(request, *args, **kwargs):
