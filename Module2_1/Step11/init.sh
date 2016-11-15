3ffabbdaf1b66c39dae4d3e1dd7cb61426421b9e
igorxut <igorxut@gmail.com>
2016-11-15 23:36:51 +0300

    Update init.sh

--- Module2_1/Step11/init.sh
+++ Module2_1/Step11/init.sh
@@ -8,14 +8,16 @@ django-admin startproject ask
 cd /home/box/web/ask
 python manage.py startapp qa
 
+cp /home/box/web/Module2_3/Step10/ask/ask/settings.py /home/box/web/ask/ask/settings.py
 cp /home/box/web/Module2_1/Step11/ask/ask/urls.py /home/box/web/ask/ask/urls.py
 cp /home/box/web/Module2_1/Step11/ask/qa/urls.py /home/box/web/ask/qa/urls.py
 cp /home/box/web/Module2_1/Step11/ask/qa/views.py /home/box/web/ask/qa/views.py
-
-cat /home/box/web/ask/ask/settings.py | sed "s/'django.contrib.staticfiles',/'django.contrib.staticfiles',\n    'qa',\n/" > temp_txt && sudo mv temp_txt /home/box/web/ask/ask/settings.py
 
 sudo ln -sf /home/box/web/Module2_1/Step11/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 
 sudo ln -sf /home/box/web/Module2_1/Step11/gunicorn.conf /etc/gunicorn.d/default
 sudo /etc/init.d/gunicorn restart
+
+cd /home/box/web/ask/
+echo >> ./ask/settings.py
