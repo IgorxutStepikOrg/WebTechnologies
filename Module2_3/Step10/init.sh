b57eb16e15f11e109ef5f362b1c9b6d3e6b5deb5
igorxut <igorxut@gmail.com>
2016-11-15 21:43:21 +0300

    Update init.sh

--- Module2_3/Step10/init.sh
+++ Module2_3/Step10/init.sh
@@ -8,14 +8,16 @@ django-admin startproject ask
 cd /home/box/web/ask/
 python manage.py startapp qa
 
+cd /home/box/web/ask/ask/
+tmp_var=`sed -n '20p;20q' settings.py`
+
+cp /home/box/web/Module2_3/Step10/ask/ask/settings.py /home/box/web/ask/ask/settings.py
 cp /home/box/web/Module2_3/Step10/ask/ask/urls.py /home/box/web/ask/ask/urls.py
 cp /home/box/web/Module2_3/Step10/ask/qa/models.py /home/box/web/ask/qa/models.py
 cp /home/box/web/Module2_3/Step10/ask/qa/urls.py /home/box/web/ask/qa/urls.py
 cp /home/box/web/Module2_3/Step10/ask/qa/views.py /home/box/web/ask/qa/views.py
 
-cat /home/box/web/ask/ask/settings.py | sed "s/'django.contrib.staticfiles',/'django.contrib.staticfiles',\n    'qa',/" > temp_txt && sudo mv temp_txt /home/box/web/ask/ask/settings.py
-cat /home/box/web/ask/ask/settings.py | sed "s/'ENGINE': 'django.db.backends.sqlite3',/'ENGINE': 'django.db.backends.mysql',/" > temp_txt && sudo mv temp_txt /home/box/web/ask/ask/settings.py
-cat /home/box/web/ask/ask/settings.py | sed "s/'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),/'NAME': 'db_ask',\n        'USER': 'user_ask',\n        'PASSWORD': 'pass_ask',\n        'HOST': 'localhost',\n        'PORT': '',/" > temp_txt && sudo mv temp_txt /home/box/web/ask/ask/settings.py
+cat /home/box/web/ask/ask/settings.py | sed "s/\# SECURITY WARNING: keep the secret key used in production secret!/\# SECURITY WARNING: keep the secret key used in production secret!\n${tmv_var}/" > temp_txt && sudo mv temp_txt /home/box/web/ask/ask/settings.py
 
 sudo ln -sf /home/box/web/Module2_3/Step10/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
@@ -29,5 +31,7 @@ mysql -u root -e "CREATE USER 'user_ask' IDENTIFIED BY 'pass_ask';"
 mysql -u root -e "GRANT ALL PRIVILEGES ON db_ask.* TO 'user_ask';"
 mysql -u root -e "FLUSH PRIVILEGES;"
 
-## cd /home/box/web/ask/
-## sudo python manage.py syncdb
+sleep 5
+
+cd /home/box/web/ask/
+sudo python manage.py syncdb
