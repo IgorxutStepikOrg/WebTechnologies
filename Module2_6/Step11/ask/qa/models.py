ff3707e3ea738889f170f173b028811e69a49fa4
igorxut <igorxut@gmail.com>
2016-11-16 15:34:15 +0300

    Update models.py

--- Module2_6/Step11/ask/qa/models.py
+++ Module2_6/Step11/ask/qa/models.py
@@ -5,85 +5,78 @@ from django.contrib.auth.models import User
 class QuestionManager(models.Manager):
 
     def new(self):
-        return self.order_by('-added_at')
+        return self.order_by("-added_at")
     
     def popular(self):
-        return self.order_by('-rating')
+        return self.order_by("-rating")
 
 
 class Question(models.Model):
-
     title = models.CharField(
         blank=False,
         default="",
-        max_length=255
+        max_length=255,
     )
     text = models.TextField(
         blank=False,
-        default=""
+        default="",
     )
     added_at = models.DateTimeField(
         auto_now_add=True,
-        blank=False
+        blank=False,
     )
     rating = models.IntegerField(
         blank=False,
-        default=0
+        default=0,
     )
     author = models.ForeignKey(
         User,
         blank=False,
-        related_name="question_author"
+        related_name="question_author",
     )
     likes = models.ManyToManyField(
         User,
         blank=True,
-        through='Likes'
+        through="Likes",
     )
     objects = QuestionManager()
     
     def get_url(self):
         return "/question/{0}/".format(self.id)
 
-    def __unicode__(self):
-        return self.text
-
-
+    
 class Likes(models.Model):
     question = models.ForeignKey(
         Question,
         blank=False,
-        related_name="like_question"
+        related_name="like_question",
     )
     user = models.ForeignKey(
         User,
         blank=False,
-        related_name="like_user"
+        related_name="like_user",
     )
     date = models.DateTimeField(
         auto_now_add=True,
-        blank=False
+        blank=False,
     )
 
 
 class Answer(models.Model):
     text = models.TextField(
         blank=False,
-        default=""
+        default="",
     )
     added_at = models.DateTimeField(
         auto_now_add=True,
-        blank=False
+        blank=False,
     )
     question = models.ForeignKey(
         Question,
-        blank=False
+        blank=False,
     )
     author = models.ForeignKey(
         User,
         blank=False,
-        related_name="answer_author"
+        related_name="answer_author",
     )
-    
-    def __unicode__(self):
-        return self.text
