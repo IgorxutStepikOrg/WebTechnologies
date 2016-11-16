e4dcee4c6ed9c49367a3e621d0916c21433a4292
igorxut <igorxut@gmail.com>
2016-11-16 11:44:06 +0300

    Update urls.py

--- Module2_5/Step8/ask/qa/urls.py
+++ Module2_5/Step8/ask/qa/urls.py
@@ -1,5 +1,10 @@
 from django.conf.urls import url
+from django.contrib import admin
+
 from qa.views import question
+
+
+admin.autodiscover()
 
 urlpatterns = [
     url(r"^(?P<num>\d+)/$", question)
