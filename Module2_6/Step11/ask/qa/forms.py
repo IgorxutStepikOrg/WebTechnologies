e69fb958a6d590466d519563aeb5858de9e65074
igorxut <igorxut@gmail.com>
2016-11-20 14:31:03 +0300

    Update forms.py

--- Module2_6/Step11/ask/qa/forms.py
+++ Module2_6/Step11/ask/qa/forms.py
@@ -4,61 +4,32 @@ from django.contrib.auth.models import User
 from qa.models import Question, Answer
 
 
-class AskForm(forms.ModelForm):
+class AskForm(forms.Form):
 
-    class Meta:
-        model = Question
-        fields = ["title", "text"]
+    title = forms.CharField(max_length=255)
+    text = forms.CharField(widget=forms.Textarea)
 
+    def save(self):
+        question = Question(**self.cleaned_data)
+        question.author_id = self._user.id
+        question.save()
+        return question
 
-class AnswerForm(forms.ModelForm):
+class AnswerForm(forms.Form):
 
-    class Meta:
-        model = Answer
-        fields = ["text", "question"]
+    text = forms.CharField(widget=forms.Textarea)
+    question = forms.IntegerField(widget=forms.HiddenInput)
 
-        
-        
-#class AskForm(forms.Form):
+    def clean_question(self):
+        question_id = self.cleaned_data["question"]
+        try:
+            question = Question.objects.get(id=question_id)
+        except Question.DoesNotExist:
+            question = None
+        return question
 
-#     title = forms.CharField(max_length=255)
-#     text = forms.CharField(widget=forms.Textarea)
-
-#     def clean(self):
-#         return self.cleaned_data
-
-#     def save(self):
-#         return Question.objects.create(**self.cleaned_data)
-
-#     author = 1
-
-#     def clean_title(self) :
-#         title = self.cleaned_data['title']
-#         return title
-
-#     def clean_text(self) :
-#         text = self.cleaned_data['text']
-#         return text
-
-#     def save(self) :
-#         quest = Question.objects.create(title=self.cleaned_data['title'], text=self.cleaned_data['text'], author=self.author)
-#         return quest
-
-
-# class AnswerForm(forms.Form):
-#     text = forms.CharField(widget=forms.Textarea)
-#     question = forms.IntegerField(widget=forms.HiddenInput)
-
-#     def clean_question(self):
-#         question_id = self.cleaned_data["question"]
-#         try:
-#             question = Question.objects.get(id=question_id)
-#         except Question.DoesNotExist:
-#             question = None
-#         return question
-
-#     def clean(self):
-#         return self.cleaned_data
-
-#     def save(self):
-#         return Answer.objects.create(**self.cleaned_data)
+    def save(self):
+        answer = Answer(**self.cleaned_data)
+        answer.author_id = self._user.id
+        answer.save()
+        return answer
